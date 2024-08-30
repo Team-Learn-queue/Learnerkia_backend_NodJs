fc42cd153d359c80c5cddc4c7f56517e24a79e4d
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2024-08-30 12:40:42 +0100

    feat: add project setup

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/'],
+};
