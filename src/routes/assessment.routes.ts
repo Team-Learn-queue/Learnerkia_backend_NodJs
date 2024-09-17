3504f1208cd9761da3fe5783e1ae6208f44f3438
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2024-09-17 17:11:26 +0100

    feat: grade submission

--- src/routes/assessment.routes.ts
+++ src/routes/assessment.routes.ts
@@ -10,12 +10,22 @@ const { createAssessment, getLearnersForAssessment } =
 const { submitAssessment } = new SubmissionController();
 const { gradeSubmission } = new GradeController();
 
-router.post("/create", upload.single("file"), createAssessment);
+// ADD INPUT VALIDATION FOR ALL ENDPOINTS
 
-router.put("/grade/:submissionId", gradeSubmission);
+router.post("/create", upload.single("file"), createAssessment); // Done
+
+// For Create Assessment
+// 1. change the "description" field to "question"
+// 2. Add a field - "highest attainable score"
+
+router.put("/grade/:submissionId", gradeSubmission); // Done
 
 router.get("/learners/:assessmentId", getLearnersForAssessment);
 
-router.post("/submit", upload.single("file"), submitAssessment);
+router.post("/submit", upload.single("file"), submitAssessment); // Done
+
+// For learners submission
+// 1. Make sure a learner cannot pass in values for "score" and "comment" fields
+// 2. Make sure a learner can only submit once.
 
 export default router;
